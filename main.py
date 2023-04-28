9fba769cda08dbfaf3d2ab6b730b4a35f6b71bb3
Morgan Jackson <morjack@siue.edu>
2023-04-28 01:25:26 -0500

    typecast

--- main.py
+++ main.py
@@ -55,4 +55,4 @@ def resume(id: str):
         return {"error": "Game ID does not exist"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", default=5000)), log_level="info")
